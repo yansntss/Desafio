35158271dc629eaee30f789826315a1e95c4409d
yansntss <yansantos.dev@gmail.com>
2021-07-10 02:56:58 -0300

    up

--- conexao.php
+++ conexao.php
@@ -1,7 +1,7 @@
 <?php 
     try{
         //faz conexão com o banco de dados
-        $conectar = new PDO("mysql:host=localhost;port=3306;dbname=dbsistema","leleco", "root");
+        $conectar = new PDO("mysql:host=localhost;port=3306;dbname=dbsistema","", "");
         echo "conectado com sucesso!";
     }catch (PDOException $e) {
         // caso ocorra erro na conexão com o banco, exibe uma msg
